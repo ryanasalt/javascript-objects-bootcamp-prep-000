d48875ec34d617096ca32a7784b047c3c65f1b7e
Ryan Saltamacchio <ryanasalt@gmail.com>
2019-05-25 23:43:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, obj, { [key]: value});
+  return Object.assign({}, playlist, { artistName: songTitle});
 };
 
 function removeFromPlaylist(obj, key){
